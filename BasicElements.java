212e7e6d5ac680b81ddddf5fd93258d4ff8b41f1
bretrandallmeyer <bretlorica09@gmail.com>
2019-01-08 12:56:34 +0800

    what were you thinking?

--- BasicElements.java
+++ BasicElements.java
@@ -1,6 +1,14 @@
+import javax.swing.JFrame;
+
 public class BasicElements {
 	
 	public static void main(String args[]){
+		JFrame myframe = new JFrame();
+
+		myframe.setDefaultCloseOperation(myFrame.EXIT_ON_CLOSE)
+		myframe.setVisible(true);
+		myframe.setSize(666, 666);
+
 		System.out.println("Basic java element");
 
 	}
